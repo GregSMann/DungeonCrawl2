ba27f4a5cbdf737480f42c833a1bff00cfb588bf
GregSMann <93481779+GregSMann@users.noreply.github.com>
2021-11-15 11:08:56 -0500

    Update ItemGenerator.java

--- ItemGenerator.java
+++ ItemGenerator.java
@@ -6,7 +6,8 @@ import java.util.Random;
 public class ItemGenerator {
     public static Item generate() {
         // TODO: replace this with your own code!
-        return new Item(ItemType.Other, "Vase", 10, 2, 0);
+        Item genItem = new Item(ItemType.Other, "Vase", 10, 2, 0);
+        return genItem;
     }
 }
 
