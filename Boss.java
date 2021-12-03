ae20292640a3dbb666ae0acbafeb37dc78c0f559
Mohammad Hossain <mhossai2@mail.umw.edu>
2021-12-03 01:04:40 +0000

    java doc for Boss.java

--- Boss.java
+++ Boss.java
@@ -1,8 +1,21 @@
+/**
+ *Creates a Boss Enemy object 
+ *
+ */
 public class Boss extends Enemy{
     private String name="Ogre Chieftan";
     private int hp=100;
     private int damage=100;
     private int protection=20;
+    /**
+     *Constructor that creates a Boss
+     @param name name of the boss
+     @param row row number of the boss (Location)
+     @param col Column number of the boss (Location)
+     @param hp Health of the boss
+     @param damage The amount of damage the boss does
+     @param protection The amount of armor the boss has
+     */
     public Boss(String name, int row, int col, int hp, int damage, int protection) {
         super(name, row, col, hp, damage, protection);
     }
